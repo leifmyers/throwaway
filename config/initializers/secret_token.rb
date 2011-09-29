5dbe68cef8f34dd6b404c6b176eb83b13cb8adc7
Leif Myers <leif.myers@takecarehealth.com>
2011-09-29 15:41:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Throwaway::Application.config.secret_token = '9b0514f01296d0d17e92ba9f3f9c41a98a550395d01dcae6858b584655cc99263ecdbf81c8ee79a21b9a2a3273691d206a8966c6ac9e1047fe245ae7a55d0527'
